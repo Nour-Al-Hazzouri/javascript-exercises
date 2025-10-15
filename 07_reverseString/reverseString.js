f1776760f10b923b079138fa970395874beddcc6
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-10-16 00:44:13 +0300

    Complete 06_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let len= string.length - 1
+    let reverse= '';
+    for (let i= len; i >= 0; i--) {
+        reverse += string[i]
+    }
+    return reverse
 
 };
 
