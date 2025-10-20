743b10a1712ba402c9821146aef4fca687969dd4
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-10-20 12:42:57 +0300

    Correct misunderstanding solution
    Included the corrected solution to the triple even assignment
    Explained the difference in approaches and where I went wrong.

--- quickExercises/sumOfTripledEvens.js
+++ quickExercises/sumOfTripledEvens.js
@@ -17,3 +17,12 @@ const sumOfTripledEvens3= array.reduce((accumulator, currentValue) => {
     if (currentValue % 2 == 0) return sum += currentValue * 3;
 });
 console.log("Sum by reduce method: " + sum);
+
+// Correction: Turns out this is the solution:
+function sumOfTripledEvens(array) {
+  return array
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 3)
+    .reduce((acc, curr) => acc + curr);
+}
+// I misunderstood by doing the same thing 3 times, but the assignment was doing the functionality once using 3 methods.
