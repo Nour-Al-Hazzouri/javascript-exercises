f84b5e49c6fe0c32e494b22f5622cfda974069cd
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-10-21 21:31:53 +0300

    Added the solution from javascript.info

---
+++ quickExercises/filterRange.js
@@ -0,0 +1,10 @@
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (matching values)
+
+alert( arr ); // 5,3,8,1 (not modified)
+
+//Note: I was solving and felt something was wrong, turns out I didn't understand the problem and was solving something else, so I discarded it.
+// Coped the solution from javascript.info 
