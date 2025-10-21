6f7e89e8690e16df9c3821a715d89c6bc5a551d0
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-10-21 18:13:48 +0300

    Including last save

--- quickExercises/camelize.js
+++ quickExercises/camelize.js
@@ -30,13 +30,13 @@ const camelize= (str) => {
 console.log(camelize(testSentence));
 
 // javascript.info solution:
-function camelize1(str) {
-  return str
-    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
-    .map(
-      // capitalizes first letters of all array items except the first one
-      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
-      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-    )
-    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
-}
+// function camelize(str) {
+//   return str
+//     .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+//     .map(
+//       // capitalizes first letters of all array items except the first one
+//       // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+//       (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+//     )
+//     .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+// }
