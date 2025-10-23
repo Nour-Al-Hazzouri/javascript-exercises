8ed8a9f4058e6e13de8ee898bbba9deb9784b138
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-10-23 07:45:33 +0300

    Making sure all changes are pushed

--- quickExercises/filterRangeInPlace.js
+++ quickExercises/filterRangeInPlace.js
@@ -6,3 +6,26 @@ function filterRangeInPlace(arr, a, b) {
     console.log(filteredArray)
 }
 filterRangeInPlace(array, 5, 10);
+
+// My results come correct but the solution did not use filter:
+/*
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    // remove if outside of the interval
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+alert( arr ); // [3, 1]
+*/
