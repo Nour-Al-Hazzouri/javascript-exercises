db2a6e1a0cb64db7e8b293aa2bfc7c55b2d9c7a4
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-03 13:57:48 +0200

    Solve 13_palindromes
    Solved 13_palindromes using a method to split based on specific characters
    Seeing the solution, the method used was fileting a variable with valid characters.
    Not sure which method is better, but I guess it would be the solution method.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (line) {
+    const transformed= line.split(/[ ,.!?]+/).join("").toLowerCase();
+    const reversed= transformed.split("").reverse().join("").toLowerCase();
+    if (transformed === reversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
