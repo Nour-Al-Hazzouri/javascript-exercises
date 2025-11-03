e36c8bffa665b36df2dc2177f4fb37b06a3b5070
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-03 12:25:58 +0200

    Solve 12_calculator
    Solved 12_calculator as intended and expected
    The solution is mostly the same too.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+	return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+	return numbers.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  }
+  let totalFactorial= number;
+	for (let i= number - 1; i > 0; i--) {
+    totalFactorial *= i;
+  }
+  return totalFactorial;
 };
 
 // Do not edit below this line
