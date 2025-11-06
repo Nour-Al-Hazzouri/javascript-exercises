c54d4672be06820b41eddb914ca5440eb62acf31
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-06 06:10:21 +0200

    Solve 16_findTheOldest
    Solved 16_findTheOldest using conditions methods where I try to consider all conditions.
    The solution was clearly better organized and built cleaner using function, one to calculate oldest and one to get age.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
 
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const oldestPerson = people.reduce((accumulator, currentPerson) => {
+        if (currentPerson.yearOfDeath !== undefined && accumulator.yearOfDeath !== undefined) 
+        {
+            return ((currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (accumulator.yearOfDeath - accumulator.yearOfBirth)) ? currentPerson : accumulator;
+        } else if (currentPerson.yearOfDeath === undefined && accumulator.yearOfDeath === undefined)
+        {
+            return ((currentYear - currentPerson.yearOfBirth) > (currentYear - accumulator.yearOfBirth)) ? currentPerson : accumulator;
+        } else if (currentPerson.yearOfDeath === undefined && accumulator.yearOfDeath !== undefined)
+        {
+            return ((currentYear - currentPerson.yearOfBirth) > (accumulator.yearOfDeath - accumulator.yearOfBirth)) ? currentPerson : accumulator;
+        } else if (currentPerson.yearOfDeath !== undefined && accumulator.yearOfDeath === undefined)
+        {
+            return ((currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (currentYear - accumulator.yearOfBirth)) ? currentPerson : accumulator;
+        }
+        }, people[0]);
+    return oldestPerson;
 };
-
+console.log(findTheOldest(people).name);
 // Do not edit below this line
 module.exports = findTheOldest;
