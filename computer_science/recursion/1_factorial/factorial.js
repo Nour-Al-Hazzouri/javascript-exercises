418cbd7f448685e24e5290277edfca51514e7b22
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-07-01 23:43:34 +0300

    Solve 01_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,8 @@
-const factorial = function() {
-
+const factorial = function (nb) {
+  if (nb < 0 || !Number.isInteger(nb)) return undefined;
+  if (nb === 1) return nb;
+  else if (nb === 0) return 1;
+  else return (nb *= factorial(nb - 1));
 };
-
 // Do not edit below this line
 module.exports = factorial;
