580501334df33af8a582711e48ec48bbd1800918
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-07-11 10:39:12 +0300

    Complete 3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,24 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (object, isRoot = true) {
+  let intCount = 0;
+  if (!Array.isArray(object) && typeof object !== "object" && isRoot === true)
+    return undefined;
+  if (Number.isInteger(object)) intCount += 1;
+  if (Array.isArray(object) || typeof object === "object") {
+    if (Array.isArray(object)) {
+      object
+        .filter((item) => Number.isInteger(item))
+        .forEach((item) => (intCount += 1));
+      object
+        .filter((item) => Array.isArray(item) || typeof item === "object")
+        .forEach((item) => (intCount += totalIntegers(item, (isRoot = false))));
+    } else if (typeof object === "object" && object !== null) {
+      for (let subObject of Object.values(object)) {
+        intCount += totalIntegers(subObject, (isRoot = false));
+      }
+    }
+  }
+  return intCount;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
