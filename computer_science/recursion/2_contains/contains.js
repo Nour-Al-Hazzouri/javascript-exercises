825a01b4692d5b637867fb4e1984db915277887f
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-07-11 05:47:37 +0300

    Complete 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,11 @@
-const contains = function() {
-  
+const contains = function (object, value) {
+  if (object === value || Number.isNaN(value)) return true;
+  if (Array.isArray(object)) return object.includes(value);
+  if (typeof object === "object" && object !== null)
+    for (let subObject of Object.values(object))
+      if (contains(subObject, value)) return true;
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
