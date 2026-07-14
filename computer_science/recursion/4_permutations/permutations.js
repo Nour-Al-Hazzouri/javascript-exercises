fd3a6dc94f1e10bd226a4d5173fa15de50e32932
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-07-14 11:06:10 +0300

    Complete 4_permutations (AI-assissted)

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,21 @@
-const permutations = function() {
-  
+const permutations = function (array, currentIndex = 0, results = []) {
+  const isArray = Array.isArray(array);
+  const arrayLength = array.length;
+  if (isArray && arrayLength === 0) return [[]];
+  else if (isArray && arrayLength === 1) return [[array[0]]];
+  else {
+    if (currentIndex === arrayLength) {
+      results.push([...array]);
+      return;
+    }
+    for (let i = currentIndex; i < arrayLength; i++) {
+      [array[currentIndex], array[i]] = [array[i], array[currentIndex]];
+      permutations(array, currentIndex + 1, results);
+      [array[currentIndex], array[i]] = [array[i], array[currentIndex]];
+    }
+    return results;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
